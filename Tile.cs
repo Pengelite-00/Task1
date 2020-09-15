c765e8d9a50b78250ab5dfecfd5f8bc7cdd2348c
Pengelite-00 <69671922+Pengelite-00@users.noreply.github.com>
2020-09-15 09:40:45 +0200

    Add files via upload

---
+++ Tile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoblinGame
+{
+
+
+    public abstract class Tile
+    {
+        protected int x;
+        protected int y;                                        //Declarations
+
+        public enum TileType { Hero, Enemy, Empty, Obstacle }
+
+        public Tile(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public int X                                         //Gets and sets for dimensions
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+    }
+}
