3ed629fee087def949171444b29434a3685561e0
Pengelite-00 <69671922+Pengelite-00@users.noreply.github.com>
2020-09-15 09:39:43 +0200

    Add files via upload

---
+++ Goblin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoblinGame
+{
+    public class Goblin : Enemy
+    {
+
+        public Goblin (int x, int y) : base (x,y,1,10,'G')                               //Constructor Aquiring variables from Enemy Class
+        {
+            
+        }
+
+        public override Movement ReturnMove(Movement move = 0)
+        {
+
+           
+
+            int rand = rnd.Next(0, 4);                          //Switch Case to determine the movement (or lack thereof) of the Goblin
+
+            switch (rand)
+            {
+                case 0:
+                    move = Movement.UP;
+                    break;
+                case 1:
+                    move = Movement.RIGHT;
+                    break;
+                case 2:
+                    move = Movement.DOWN;
+                    break;
+                case 3:
+                    move = Movement.LEFT;
+                    break;
+                case 4:
+                    move = Movement.NO_MOVEMENT;
+                    break;
+            }
+
+            return move;
+        }
+
+
+    }
+}
---
+++ Hero.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoblinGame
+{
+   public class Hero : Character
+    {
+        public Hero (int x, int y, int hp, int damage) : base(x,y,'H')                 //Constructor deriving variables from Character class
+        {
+            this.HP = hp;
+            this.maxHP = hp;
+            this.damage = 2;
+        }
+
+        public override Movement ReturnMove(Movement move)                                      //Movement Method
+        {
+            bool canMove = false;
+            switch(move)                                         //Switch case to determine if Hero can move in intended direction
+            {
+                case Movement.UP:
+                    if (characterVision[0].GetType().Name.Equals(nameof(EmptyTile))) { canMove = true; }
+                    break;
+                case Movement.RIGHT:
+                    if (characterVision[1].GetType().Name.Equals(nameof(EmptyTile))) { canMove = true; }
+                    break;
+                case Movement.DOWN:
+                    if (characterVision[2].GetType().Name.Equals(nameof(EmptyTile))) { canMove = true; }
+                    break;
+                case Movement.LEFT:
+                    if (characterVision[3].GetType().Name.Equals(nameof(EmptyTile))) { canMove = true; }
+                    break;
+            }
+
+            if (canMove)
+            {
+                return move;
+            }
+
+            else
+            {
+                return Movement.NO_MOVEMENT;
+            }
+
+
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+
+    }
+}
