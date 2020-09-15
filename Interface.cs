f88a55b619004d8528deef0c483dc6bbce163994
Pengelite-00 <69671922+Pengelite-00@users.noreply.github.com>
2020-09-15 09:40:06 +0200

    Add files via upload

---
+++ Interface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoblinGame
+{
+    public partial class Interface : Form
+    {
+
+        private GameEngine gameEngine;
+
+
+        public Interface()
+        {
+            InitializeComponent();
+
+            
+
+            
+
+        }
+
+        public void Populate(int minWidth, int maxWidth, int minHeight, int maxHeight, int numOfEnemies)
+        {
+            gameEngine = new GameEngine(minWidth, maxWidth, minHeight, maxHeight, numOfEnemies);
+        }
+
+        public void UpdateForm()
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = gameEngine.ToString();
+        }
+    }
+}
