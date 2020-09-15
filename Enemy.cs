5b2c7b6fe311b299773b3341a08196c584a9e857
Pengelite-00 <69671922+Pengelite-00@users.noreply.github.com>
2020-09-15 09:39:19 +0200

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoblinGame
+{
+    public abstract class Enemy : Character
+    {
+        protected Random rnd;
+
+        public Enemy (int x, int y, int enemyDamage, int hp, char symbol) : base(x,y,symbol)               //Constructor deriving variables from Character
+        {
+            this.damage = enemyDamage;
+            this.HP = hp;
+            this.maxHP = hp;
+            
+        }
+
+        public override string ToString()
+        {
+            return this.GetType().Name + "at" + this.X + this.Y + this.damage;                      //ToString() overring method to display Enemy Stats
+        }
+
+    }
+}
---
+++ GameEngine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoblinGame
+{
+    class GameEngine
+    {
+        private Map gameEngineMap;
+        private static readonly char Hero = 'H';
+        private static readonly char Empty = ' ';
+        private static readonly char Goblin = 'G';                  //Declarations
+        private static readonly char Obstacle = 'X';
+
+        public GameEngine(int minWidth, int maxWidth, int minHeight, int maxHeight, int numOfEnemies)     //Constructor deriving variables from Map class
+        {
+            gameEngineMap = new Map(minWidth, maxWidth, minHeight, maxHeight, numOfEnemies);
+        }
+
+        public Map Map
+        {
+            get { return gameEngineMap; }
+            set { gameEngineMap = value; }
+        }
+
+        public override string ToString()               //Method inteded to create 2D map, can't get mapHeight to register outside of the Map class unfortunately
+        {
+            string output = "";
+
+            for (int i = 0; i < gameEngineMap.mapHeight; i++)
+            {
+                for (int j = 0; j < gameEngineMap.MapWidth; j++)
+                {
+                    switch (gameEngineMap.MapTiles[j, i].GetType().Name)
+                    {
+                        case nameof(Empty):
+                            output += Empty;
+                            break;
+                        case nameof(Obstacle):
+                            output += Obstacle;
+                            break;
+                        case nameof(Hero):
+                            output += Hero;
+                            break;
+                        case nameof(Goblin):
+                            output += Goblin;
+                            break;
+                    }
+                    
+                }
+                output += "\n";                  //Output to go to next line
+            }
+
+            return output;            //End of Method
+        }
+    }
+}
